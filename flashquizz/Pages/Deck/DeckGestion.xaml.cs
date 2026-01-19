2a8556d7df6d05ddf8580f4294146987da1625e2
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-01-19 22:13:44 +0100

    feat(deckgestion): add deck button nav page

--- flashquizz/Pages/Deck/DeckGestion.xaml.cs
+++ flashquizz/Pages/Deck/DeckGestion.xaml.cs
@@ -1,4 +1,4 @@
-namespace flashquizz.Pages;
+namespace flashquizz.Pages.Deck;
 
 public partial class DeckGestion : ContentPage
 {
@@ -6,11 +6,15 @@ public partial class DeckGestion : ContentPage
     {
         InitializeComponent();
     }
-
+    
     private async void OnBackClicked(object sender, EventArgs e)
     {
         // Revenir à la page précédente
         if (Navigation.NavigationStack.Count > 1)
             await Navigation.PopAsync();
+    }
+    private async void OnClickedAddDeck(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DeckAdd());
     }
 }
