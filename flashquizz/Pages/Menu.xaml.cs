316890265ab62a2122e14f24cbedf31ec52057d1
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-01-19 11:18:29 +0100

    feat(menu): navigation vers le deck

--- flashquizz/Pages/Menu.xaml.cs
+++ flashquizz/Pages/Menu.xaml.cs
@@ -5,15 +5,24 @@ public partial class Menu : ContentPage
 	public Menu()
 	{
 		InitializeComponent();
+        
 	}
 
     private async void OnClickedGestionDeck(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Menu());
+        await Navigation.PushAsync(new DeckGestion());
+
     }
 
     private async void OnClickedPlay(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Menu());
+        await Shell.Current.GoToAsync("play");
+    }
+
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        // Vérifie si on peut revenir en arrière
+        if (Navigation.NavigationStack.Count > 1)
+            await Navigation.PopAsync();
     }
 }
