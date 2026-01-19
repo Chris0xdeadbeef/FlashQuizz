277db675d26dc339564443430f9f516e47f6f78e
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-01-19 22:13:19 +0100

    feat(deckchoice): back button

--- flashquizz/Pages/Play/DeckChoice.xaml.cs
+++ flashquizz/Pages/Play/DeckChoice.xaml.cs
@@ -4,6 +4,12 @@ public partial class DeckChoice : ContentPage
 {
 	public DeckChoice()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        // Revenir à la page précédente
+        if (Navigation.NavigationStack.Count > 1)
+            await Navigation.PopAsync();
+    }
 }
