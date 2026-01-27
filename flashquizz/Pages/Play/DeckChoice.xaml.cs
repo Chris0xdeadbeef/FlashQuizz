55fa3e065a65f1bad7a773fc985de00069b53551
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2026-01-27 22:30:06 +0100

    doc(deckchoice): ajout commentaires

--- flashquizz/Pages/Play/DeckChoice.xaml.cs
+++ flashquizz/Pages/Play/DeckChoice.xaml.cs
@@ -6,19 +6,30 @@ namespace flashquizz.Pages.Play;
 public partial class DeckChoice : ContentPage
 {
     private readonly DeckService _deckService;
+
+    /// <summary>
+    /// Liste observable des decks disponibles pour le mode "Play".
+    /// </summary>
+    public ObservableCollection<Models.Deck> Decks => _deckService.Decks;
+
+    /// <summary>
+    /// Initialise la page permettant de choisir un deck pour jouer.
+    /// </summary>
     public DeckChoice(DeckService deckService)
-	{        
+    {
         _deckService = deckService;
 
         BindingContext = this;
 
         InitializeComponent();
     }
+
+    /// <summary>
+    /// Retourne à la page précédente si possible.
+    /// </summary>
     private async void OnBackClicked(object sender, EventArgs e)
     {
-        // Revenir à la page précédente
         if (Navigation.NavigationStack.Count > 1)
             await Navigation.PopAsync();
     }
-    public ObservableCollection<Models.Deck> Decks => _deckService.Decks;
 }
